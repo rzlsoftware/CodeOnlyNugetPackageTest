5c58edb3183758e42e139bd658928bc53ff9c3b6
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2021-03-25 18:35:22 +0100

    add empty usercontrol that breaks the code

---
+++ src/TheWPFClient/UserControl__that_breaks_the_code.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheWPFClient
+{
+    /// <summary>
+    /// Interaction logic for UserControl__that_breaks_the_code.xaml
+    /// </summary>
+    public partial class UserControl__that_breaks_the_code : UserControl
+    {
+        public UserControl__that_breaks_the_code()
+        {
+            InitializeComponent();
+        }
+    }
+}
