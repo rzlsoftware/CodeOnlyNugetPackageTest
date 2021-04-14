b3f21b1b856c8a7db7bf60d498de6ccc932389fd
Jakob Ferdinand Wegenschimmel <jwegenschimmel@rzl.at>
2021-04-14 10:07:46 +0200

    recreate issue with Nullable

--- src/TheWPFClient/Program.cs
+++ src/TheWPFClient/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TheWPFClient
 {
     class Program
     {
+        [AllowNull] private readonly string OptionalValue;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello beatiful World!");
-
-            var text = RZL.TheImportantLibrary.DoSomething();
         }
     }
 }
